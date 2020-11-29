d51156ef473d1adb76ff10b70bc3a0ede3c2f0eb
Ahmad Naufal Hilmy <hilmyahmadnaufal@gmail.com>
2020-11-29 22:27:09 +0700

    feat: create peserta service

---
+++ src/main/java/apap/tugas/sipelatihan/service/PesertaServiceImpl.java
@@ -0,0 +1,43 @@
+package apap.tugas.sipelatihan.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import apap.tugas.sipelatihan.model.PelatihanModel;
+import apap.tugas.sipelatihan.model.PesertaModel;
+import apap.tugas.sipelatihan.model.PesertaPelatihanModel;
+import apap.tugas.sipelatihan.repository.PesertaDb;
+
+@Service
+@Transactional
+public class PesertaServiceImpl implements PesertaService {
+
+    @Autowired
+    PesertaDb pesertaDb;
+
+    @Override
+    public List<PesertaPelatihanModel> assignManyPesertaToPelatihan(List<PesertaModel> peserta,
+            PelatihanModel pelatihan) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public PesertaModel inputValidation(PesertaModel peserta) {
+        if (peserta.getAlamat().length() > 100 || peserta.getDepartemen().length() > 100
+                || peserta.getNo_telepon().length() > 20 || peserta.getNama_peserta().length() > 100) {
+            throw new IllegalArgumentException("Input tidak valid");
+        }
+        return peserta;
+    }
+
+    @Override
+    public PesertaModel create(PesertaModel peserta) {
+        inputValidation(peserta);
+        return pesertaDb.save(peserta);
+    }
+}
