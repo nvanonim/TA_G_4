a72c4cfc452ec183bb3c3db0fe3b42b2d7b4ce5b
Alghifari Taufan Nugroho <43292197+nvanonim@users.noreply.github.com>
2020-12-15 01:36:24 +0700

    Alghi/Webservice-membuat-pelatihan (#15)
    * feat: add webservice for creating pelatihan
    
    add cors and csrf disable on WebSecurityConfig
    create restcontroller for pelatihan
    create restservice for pelatihan

---
+++ src/main/java/apap/tugas/sipelatihan/restcontroller/PelatihanRestController.java
@@ -0,0 +1,35 @@
+package apap.tugas.sipelatihan.restcontroller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import apap.tugas.sipelatihan.model.PelatihanModel;
+import apap.tugas.sipelatihan.restservice.PelatihanRestService;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1")
+public class PelatihanRestController {
+
+    @Autowired
+    private PelatihanRestService pelatihanRestService;
+    
+    @PostMapping("/pelatihan")
+    private PelatihanModel createPelatihan(@Valid @RequestBody PelatihanModel pelatihan,
+            BindingResult bindingResult
+    ) {
+        if (bindingResult.hasFieldErrors()) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Request body has invalid type or missing field"
+            );
+        }
+        else {
+            return pelatihanRestService.createPelatihan(pelatihan);
+        }
+    }
+}
