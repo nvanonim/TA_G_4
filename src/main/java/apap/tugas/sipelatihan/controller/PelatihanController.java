6c038270ab514b816aa7200a3fb504bb85eca51c
Ahmad Naufal Hilmy <hilmyahmadnaufal@gmail.com>
2020-12-16 12:52:40 +0700

    feat: add tambah peserta controller

--- src/main/java/apap/tugas/sipelatihan/controller/PelatihanController.java
+++ src/main/java/apap/tugas/sipelatihan/controller/PelatihanController.java
@@ -7,6 +7,7 @@ import apap.tugas.sipelatihan.model.UserModel;
 import apap.tugas.sipelatihan.service.UserService;
 import apap.tugas.sipelatihan.service.JenisPelatihanService;
 import apap.tugas.sipelatihan.service.PelatihanService;
+import apap.tugas.sipelatihan.service.PesertaService;
 //import apap.tugas.sipelatihan.service.PesertaService;
 import apap.tugas.sipelatihan.service.TrainerService;
 
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -34,31 +36,80 @@ public class PelatihanController {
 
     @Autowired
     private UserService userService;
-//
-//    @Autowired
-//    private PesertaService pesertaService;
+
+    @Autowired
+    private PesertaService pesertaService;
+
+    @GetMapping("")
+    public String index() {
+        return "pelatihan/index";
+    }
+
+    @GetMapping("/{id}/add/peserta")
+    public String formTambahPesertaPelatihan(@PathVariable Long id, Model model) {
+        PelatihanModel container = pelatihanService.getPelatihanById(id);
+        container.setListPesertaPelatihan(new ArrayList<>());
+        container.getListPesertaPelatihan().add(new PesertaPelatihanModel());
+        model.addAttribute("pelatihan", container);
+        List<PesertaModel> listPeserta = pesertaService.getAllPeserta();
+        model.addAttribute("listPeserta", listPeserta);
+        return "pelatihan/add-peserta-pelatihan";
+    }
+
+    @PostMapping(value = "/{id}/add/peserta", params = "add-row")
+    public String formTambahPesertaPelatihanAddRow(@PathVariable Long id, @ModelAttribute PelatihanModel pelatihan,
+            Model model) {
+        pelatihan.getListPesertaPelatihan().add(new PesertaPelatihanModel());
+        model.addAttribute("pelatihan", pelatihan);
+        List<PesertaModel> listPeserta = pesertaService.getAllPeserta();
+        model.addAttribute("listPeserta", listPeserta);
+        return "pelatihan/add-peserta-pelatihan";
+    }
+
+    @PostMapping(value = "/{id}/add/peserta", params = "delete-row")
+    public String formTambahPesertaPelatihanDeleteRow(@PathVariable Long id,
+            @ModelAttribute("pelatihan") PelatihanModel pelatihan, @RequestParam("delete-row") int value, Model model) {
+        pelatihan.getListPesertaPelatihan().remove(value);
+        model.addAttribute("pelatihan", pelatihan);
+        List<PesertaModel> listPeserta = pesertaService.getAllPeserta();
+        model.addAttribute("listPeserta", listPeserta);
+        return "pelatihan/add-peserta-pelatihan";
+    }
+
+    @PostMapping(value = "/{id}/add/peserta")
+    public String formTambahPesertaPelatihanDeleteRow(@PathVariable Long id,
+            @ModelAttribute("pelatihan") PelatihanModel pelatihan, Model model) {
+        try {
+            List<PesertaPelatihanModel> pesertaPelatihan = pesertaService
+                    .assignManyPesertaToPelatihan(pelatihan.getListPesertaPelatihan(), pelatihan);
+
+            model.addAttribute("pesertaPelatihan", pesertaPelatihan);
+        } catch (Exception e) {
+            model.addAttribute("error_msg", e.getMessage());
+            model.addAttribute("pelatihan", pelatihan);
+            model.addAttribute("listPeserta", pesertaService.getAllPeserta());
+            return "pelatihan/add-peserta-pelatihan";
+        }
+        return "pelatihan/success-add-peserta";
+    }
 
     @GetMapping("/view/{id}")
-    public String viewDetailPelatihan(
-            @PathVariable Long id, Model model) {
+    public String viewDetailPelatihan(@PathVariable Long id, Model model) {
 
         PelatihanModel pelatihan = pelatihanService.getPelatihanById(id);
         List<PesertaPelatihanModel> listPeserta = pelatihan.getListPesertaPelatihan();
-        if (!(listPeserta.isEmpty())){
+        if (!(listPeserta.isEmpty())) {
             model.addAttribute("title", "Daftar Peserta");
             model.addAttribute("status", true);
 
-
-        }else{
+        } else {
             model.addAttribute("status", false);
 
         }
         model.addAttribute("pelatihan", pelatihan);
         model.addAttribute("listPeserta", listPeserta);
 
-
         return "/pelatihan/view-pelatihan";
-
 
     }
 
@@ -72,10 +123,8 @@ public class PelatihanController {
     }
 
     @PostMapping("/add")
-    public String addPelatihanSubmit(@ModelAttribute("pelatihan") PelatihanModel pelatihan,
-            Authentication auth,
-            Model model
-        ) {
+    public String addPelatihanSubmit(@ModelAttribute("pelatihan") PelatihanModel pelatihan, Authentication auth,
+            Model model) {
 
         System.out.println("-------------------------------------------------------------");
         System.out.println(pelatihan.getWaktu_mulai());
@@ -84,13 +133,11 @@ public class PelatihanController {
 
         if (pelatihan.getTanggal_mulai().after(pelatihan.getTanggal_selesai())) {
             return "redirect:/pelatihan/add?errort";
-        }
-        else {
-            if(pelatihan.getWaktu_mulai().after(pelatihan.getWaktu_selesai()) || pelatihan.getWaktu_mulai()
-                    .equals(pelatihan.getWaktu_selesai())) {
+        } else {
+            if (pelatihan.getWaktu_mulai().after(pelatihan.getWaktu_selesai())
+                    || pelatihan.getWaktu_mulai().equals(pelatihan.getWaktu_selesai())) {
                 return "redirect:/pelatihan/add?errorw";
-            }
-            else {
+            } else {
                 UserModel pengaju = userService.getUserByUsername(auth.getName());
                 pelatihan.setPengaju(pengaju);
                 pelatihanService.addPelatihan(pelatihan);
