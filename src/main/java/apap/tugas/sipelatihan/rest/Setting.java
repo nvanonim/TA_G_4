8c3a46ceae9dac387e0187fddac3ed238d11e2e3
ryananshar <ansharryan@gmail.com>
2020-12-17 21:58:51 +0700

    chore: add url to resolve conflict

--- src/main/java/apap/tugas/sipelatihan/rest/Setting.java
+++ src/main/java/apap/tugas/sipelatihan/rest/Setting.java
@@ -2,4 +2,6 @@ package apap.tugas.sipelatihan.rest;
 
 public class Setting {
     final public static String pegawaiUrl = "https://si-pegawai.herokuapp.com";
+
+    final public static String laporanUrl = "https://57a9975c-0e28-4bce-b951-ffedf0734595.mock.pstmn.io/";
 }
